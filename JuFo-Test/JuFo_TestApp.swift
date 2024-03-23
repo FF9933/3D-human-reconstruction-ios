96c6f43b31b2e5f733b7b86526161d512494405f
Viktor Pantushev <vik.pa@gmx.com>
2024-03-23 13:27:20 +0100

    Initial Commit

---
+++ JuFo-Test/JuFo_TestApp.swift
@@ -0,0 +1,17 @@
+//
+//  JuFo_TestApp.swift
+//  JuFo-Test
+//
+//  Created by viktor on 23.03.24.
+//
+
+import SwiftUI
+
+@main
+struct JuFo_TestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
